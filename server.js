c6a6de4b026bc579c58ffd56ceb02997a727573e
abhishek <abhishek.74734@mygyanvihar.com>
2024-01-17 17:18:40 +0530

    CORS headers

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
 })
 
 // routes
