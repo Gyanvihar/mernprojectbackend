5312c8032260f00a03462088ad9f46b217390a10
abhishek <abhishek.74734@mygyanvihar.com>
2024-01-17 17:04:31 +0530

    vercel deployment

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.use((req, res, next) => {
 app.use('/api/workouts', workoutRoutes)
 app.use('/api/user', userRoutes)
 
+app.get('/', (req, res) => {
+  res.send("connected");
+})
+
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
